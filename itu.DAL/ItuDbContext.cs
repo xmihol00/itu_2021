0254e3b70055f194e0ff95a1640fd22b49a2be76
dejv.mihola <david.mihola@sperling.cz>
2021-10-26 12:51:34 +0200

    seed

--- itu.DAL/ItuDbContext.cs
+++ itu.DAL/ItuDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using itu.DAL.Entities;
+using itu.DAL.Entities.Tasks;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,20 @@ namespace itu.DAL
     public class ItuDbContext : DbContext
     {
         public ItuDbContext(DbContextOptions<ItuDbContext> options) : base(options) { }
+
+        public DbSet<TaskEntity> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ContractEntity>().ToTable("Contracts");
+            modelBuilder.Entity<AcceptationEntity>().ToTable("Acceptations");
+            modelBuilder.Entity<ArchivationEntity>().ToTable("Archivations");
+            modelBuilder.Entity<AssessmentEntity>().ToTable("Assessments");
+            modelBuilder.Entity<AssignmentEntity>().ToTable("Assignments");
+            modelBuilder.Entity<EstimateEntity>().ToTable("Estimates");
+            modelBuilder.Entity<PublishEntity>().ToTable("Publishes");
+        }
     }
 }
--- itu.DAL/Seeds/TaskSeed.cs
+++ itu.DAL/Seeds/TaskSeed.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using itu.DAL.Entities;
+using itu.DAL.Entities.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace itu.DAL.Seeds
 {
     public static class TaskSeed
     {
+        private static readonly List<TaskEntity> _task = new List<TaskEntity>()
+        {
+
+        };
+
+        public static void SeedTasks(this ModelBuilder modelBuilder)
+        {
+            foreach (TaskEntity task in _task)
+            {
+                if (task.GetType() == typeof(TaskEntity))
+                {
+                    modelBuilder.Entity<TaskEntity>().HasData(task);
+                }
+                else if (task.GetType() == typeof(ContractEntity))
+                {
+                    modelBuilder.Entity<ContractEntity>().HasData(task);
+                }
+                else if (task.GetType() == typeof(AcceptationEntity))
+                {
+                    modelBuilder.Entity<AcceptationEntity>().HasData(task);
+                }
+                else if (task.GetType() == typeof(ArchivationEntity))
+                {
+                    modelBuilder.Entity<ArchivationEntity>().HasData(task);
+                }
+                else if (task.GetType() == typeof(AssessmentEntity))
+                {
+                    modelBuilder.Entity<AssessmentEntity>().HasData(task);
+                }
+                else if (task.GetType() == typeof(AssignmentEntity))
+                {
+                    modelBuilder.Entity<AssignmentEntity>().HasData(task);
+                }
+                else if (task.GetType() == typeof(EstimateEntity))
+                {
+                    modelBuilder.Entity<EstimateEntity>().HasData(task);
+                }
+                else if (task.GetType() == typeof(PublishEntity))
+                {
+                    modelBuilder.Entity<PublishEntity>().HasData(task);
+                }
+            }
+        }
     }
 }
